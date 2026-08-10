6b8173b70b3be26b44055da3b5f5e363edaab172
Priya Selvakumar <pavipriya507@gmail.com>
2026-08-10 05:45:24 +0000

    soc: microchip: add PIC32CM LS00 initial support

---
+++ boards/microchip/pic32c/pic32cm_ls00_cnano/board.cmake
@@ -0,0 +1,4 @@
+# Copyright (c) 2026 Microchip Technology Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+# Flash/debug runner will be added after the first successful build.
---
+++ soc/microchip/pic32c/pic32cm_ls/common/soc.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2026 Microchip Technology Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file soc.c
+ * @brief Microchip PIC32CM LS family initialization code
+ */
+
+void soc_reset_hook(void)
+{
+}
